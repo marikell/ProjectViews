6106dced9825f3d8c31b471594aa512aabb8c8c0
Marianne K. A Silva <marikell_silva@hotmail.com>
2017-08-24 07:20:29 -0300

    Algumas modificacoes

--- ProjectViewTeste/App_Start/BundleConfig.cs
+++ ProjectViewTeste/App_Start/BundleConfig.cs
@@ -23,9 +23,11 @@ namespace ProjectViewTeste
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            //bundles.Add(new ScriptBundle("~/Content/js").Include("~/Content/login.js"));
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css", "~/Content/login.css"));
         }
     }
 }
