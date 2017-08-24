a852e662211d0438bce7346a86c60bc567abd103
marikell <marikell_silva@hotmail.com>
2017-08-24 08:39:55 -0300

    Mod na fazsoft

--- ProjectViewTeste/App_Start/BundleConfig.cs
+++ ProjectViewTeste/App_Start/BundleConfig.cs
@@ -23,8 +23,8 @@ namespace ProjectViewTeste
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            //bundles.Add(new ScriptBundle("~/Content/js").Include("~/Content/login.js"));
-            
+            bundles.Add(new ScriptBundle("~/Content/js").Include("~/Content/login.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css", "~/Content/login.css"));
