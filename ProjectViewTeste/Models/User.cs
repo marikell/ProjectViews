faaa09e660c33d327bcefdde26aae65b639eef87
Marianne K. A Silva <marikell_silva@hotmail.com>
2017-08-26 20:20:18 -0300

    260817

---
+++ ProjectViewTeste/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectViewTeste.Models
+{
+    public class User
+    {
+        
+        public int NidUser { get; set; }
+        public string  NomUser { get; set; }
+        public string DesSenha { get; set; }
+
+    }
+}
